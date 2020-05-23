0619e1a182b07678f561c92f436c49dd8414199c
raisercostin <raisercostin@gmail.com>
2020-05-23 11:03:25 +0300

    scraping

--- src/main/java/org/raisercostin/jscraper/JScraper.java
+++ src/main/java/org/raisercostin/jscraper/JScraper.java
@@ -15,7 +15,12 @@ import scala.concurrent.Future;
 
 public class JScraper {
   public static void scrape(PathLocation crawledDir) {
-    crawledDir.parent().get().findFilesAndDirs(false).doOnNext(x -> scrapePage(x)).blockLast();
+    crawledDir.parent()
+      .get()
+      .ls()
+      .filter(x -> !x.filename().startsWith("."))
+      .take(1)
+      .forEach(x -> scrapePage(x));
     //scrapePage(crawledDir);
   }
 
@@ -24,7 +29,7 @@ public class JScraper {
     MetaInfo meta = page.readMeta();
     if (meta.httpResponseHeaderContentTypeIsHtml()) {
       Future<Seq<Listing>> res = new NamekConsole(null).extractPage(Locations.file(page.absoluteAndNormalized()));
-      Seq<Listing> result = Await.result(res, scala.concurrent.duration.Duration.apply(10, TimeUnit.SECONDS));
+      Seq<Listing> result = Await.result(res, scala.concurrent.duration.Duration.apply(1000, TimeUnit.SECONDS));
       System.out.println(Nodes.json.toString(result));
     }
   }
